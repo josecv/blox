a7afafcf286be37412edd74df2a2f7def81d4838
Jose <jose.cortesvarela@mail.utoronto.ca>
2017-02-16 13:14:02 -0500

    Do a chdir so we can run the game from anywhere.

--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,23 @@
 #include "Logger.h"
 #include "Blox.h"
 
+#ifndef __WIN32
+#include <unistd.h>
+#endif
+
+#ifndef GAME_HOME
+  #define GAME_HOME "."
+#endif /* GAME_HOME */
+
 using namespace blox;
 
+static void chdir_to_home() {
+  chdir(GAME_HOME);
+  printf("%s\n", GAME_HOME);
+}
+
 int main(int argc, char **argv) {
+  chdir_to_home();
   Logger::init("blox.log");
   Blox blox;
   blox.run();
